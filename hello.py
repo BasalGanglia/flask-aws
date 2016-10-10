253c2aa3c842e0706b06a1a6eac11a8b06b201e7
Ilkka Kosunen <ilkka.kosunen@gmail.com>
2016-10-10 13:57:30 +0000

    added some html template stuff

--- hello.py
+++ hello.py
@@ -1,14 +1,39 @@
 import os
-from flask import Flask
+
+from flask import Flask, request, render_template, redirect, url_for, flash
 
 app = Flask(__name__)
 
-@app.route('/')
-def hello_world():
-    return 'Hello World!'
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    error = None
+    if request.method == 'POST':
+        if valid_login(request.form['username'], request.form['password']):
+            flash("Succesfully logged in")
+            return redirect(url_for('welcome',username=request.form.get('username')))
+        else:
+            error = 'Incorrect username and password'
+    return render_template('login.html',error=error)
+
+def valid_login(username,password):
+    if username == password:
+        return True
+    else:
+        return False
+        
+@app.route('/welcome/<username>')
+def welcome(username):
+    return render_template('welcome.html', username=username)
     
+        
+    
+    
+    
+
 if __name__ == '__main__':
     host = os.getenv('IP' , '0.0.0.0')
     port = int(os.getenv('PORT', 5000))
+    app.debug = True
+    app.secret_key = 'SuperSecretKey'
     app.run(host= host, port=port)
     
